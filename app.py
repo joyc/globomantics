2342ea5693462c1bc4c71bf064292ee3d9c7b9ec
joyc <joyc@live.jp>
2020-04-13 17:09:32 +0900

    add db files

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, g
+import sqlite3
 import pdb
 
 app = Flask(__name__)
@@ -13,14 +14,43 @@ def home():
 
 @app.route("/item/new", methods=["GET", "POST"])
 def new_item():
+    conn = get_db()
+    c = conn.cursor()
+
     # pdb.set_trace()
     if request.method == "POST":
         # Process the from data
-        print("From data:")
-        print("Title: {}, Description: {}".format(
-            request.form.get('title'), request.form.get('description')
-        ))
+        # print("From data:")
+        # print("Title: {}, Description: {}".format(
+        #     request.form.get('title'), request.form.get('description')
+        # ))
+        c.execute("""INSERT INTO items
+                    (title, description, price, image, category_id, subcategory_id)
+                        VALUES (?,?,?,?,?,?)""",
+                        (
+                            request.form.get("title"),
+                            request.form.get("description"),
+                            float(request.form.get("price")),
+                            "",
+                            1,
+                            1
+                        )
+        )
+        conn.commit()
         # Redirect to top page
         return redirect(url_for("home"))
-        
+
     return render_template("new_item.html")
+
+def get_db():
+    db = getattr(g, "_database", None)
+    if db is None:
+        db = g._database = sqlite3.connect("db/globomantics.db")
+    return db
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, "_database", None)
+    if db is not None:
+        db.close()
+
