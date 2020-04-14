ca969410fe637ec789cbad3de5a27c6e30e34377
joyc <joyc@live.jp>
2020-04-14 11:19:17 +0900

    add Flask-WTF

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
 from flask import Flask, render_template, request, redirect, url_for, g, flash
-import sqlite3
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextAreaField, SubmitField
 import pdb
+import sqlite3
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "secretkey"
+
+class NewItemForm(FlaskForm):
+    title       = StringField("Title")
+    price       = StringField("Price")
+    description = TextAreaField("Description")
+    submit      = SubmitField("Submit")
 
 @app.route("/")
 def home():
@@ -41,6 +49,7 @@ def home():
 def new_item():
     conn = get_db()
     c = conn.cursor()
+    form = NewItemForm()
 
     # pdb.set_trace()
     if request.method == "POST":
@@ -53,9 +62,9 @@ def new_item():
                     (title, description, price, image, category_id, subcategory_id)
                         VALUES (?,?,?,?,?,?)""",
                         (
-                            request.form.get("title"),
-                            request.form.get("description"),
-                            float(request.form.get("price")),
+                            form.title.data,
+                            form.description.data,
+                            float(form.price.data),
                             "",
                             1,
                             1
@@ -66,7 +75,7 @@ def new_item():
         flash("Item {} has been successfully submitted".format(request.form.get("title")), "success")
         return redirect(url_for("home"))
 
-    return render_template("new_item.html")
+    return render_template("new_item.html", form=form)
 
 def get_db():
     db = getattr(g, "_database", None)
