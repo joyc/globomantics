195ae825c26242643370b078d1c78b9c98b9868c
joyc <joyc@live.jp>
2020-04-19 00:58:31 +0900

    ajax async comment

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, jsonify, send_from_directory, render_template, request, redirect, url_for, g, flash
 from flask_wtf import FlaskForm, RecaptchaField
 from flask_wtf.file import FileAllowed, FileRequired
-from wtforms import FileField, StringField, TextAreaField, SubmitField, SelectField, DecimalField
+from wtforms import HiddenField, FileField, StringField, TextAreaField, SubmitField, SelectField, DecimalField
 from wtforms.validators import InputRequired, DataRequired, Length, ValidationError
 from wtforms.widgets import Input
 from werkzeug.utils import secure_filename, escape, unescape
@@ -200,6 +200,37 @@ class FilterForm(FlaskForm):
     )
     submit      = SubmitField("Filter")
 
+class NewCommentForm(FlaskForm):
+    content = TextAreaField("Comment", validators=[InputRequired("Input is required."), DataRequired("Data is required.")])
+    item_id = HiddenField(validators=[DataRequired()])
+    submit  = SubmitField("Submit")
+
+@app.route("/comment/new", methods=["POST"])
+def new_comment():
+    conn = get_db()
+    c = conn.cursor()
+    form = NewCommentForm()
+
+    # 判断是否是Ajax提交
+    try:
+        is_ajax = int(request.form["ajax"])
+    except:
+        is_ajax = 0
+
+    if form.validate_on_submit():
+        c.execute("""INSERT INTO comments (content, item_id)
+                     VALUES (?, ?)""",
+                     (
+                         escape(form.content.data),
+                         form.item_id.data
+                     )
+        )
+        conn.commit()
+        if is_ajax:
+            return render_template("_comment.html", content=form.content.data)
+    if is_ajax:
+        return " Content is required.", 404
+    return redirect(url_for('item', item_id=form.item_id.data))
 
 @app.route("/")
 def home():
@@ -343,9 +374,22 @@ def item(item_id):
         item = {}
 
     if item:
+        # add show comments
+        comments_from_db = c.execute("""SELECT content FROM comments
+                    WHERE item_id = ? ORDER BY id DESC""", (item_id,)
+        )
+        comments = []
+        for row in comments_from_db:
+            comment = {
+                "content": row[0]
+            }
+            comments.append(comment)
+        commentForm    = NewCommentForm()
+        commentForm.item_id.data = item_id
+
         deleteItemForm = DeleteItemForm()
 
-        return render_template("item.html", item=item, deleteItemForm=deleteItemForm)
+        return render_template("item.html", commentForm=commentForm, item=item, comments=comments, deleteItemForm=deleteItemForm)
     return redirect(url_for("home"))
 
 
